fbc281b9b7688d767e4cc13c3d84f75b91de3aa2
Kishlin <hello@pierrelouislegrand.fr>
2019-04-12 22:43:01 +0200

    Feature: FizzBuzz test and source classes.

--- src/FizzBuzzKata/FizzBuzz.php
+++ src/FizzBuzzKata/FizzBuzz.php
@@ -1,1 +1,29 @@
+<?php
 
+namespace TDD\FizzBuzzKata;
+
+class FizzBuzz
+{
+    private const TRIGGER_FIZZ = 3;
+
+    private const TRIGGER_BUZZ = 5;
+
+    public function __invoke(int $number): string
+    {
+        if (1 > $number || $number > 100) {
+            throw new \InvalidArgumentException("$number is not a valid number between 1 and 100.");
+        }
+
+        $ret = '';
+
+        if (0 === $number%self::TRIGGER_FIZZ) {
+            $ret .= 'Fizz';
+        }
+
+        if (0 === $number%self::TRIGGER_BUZZ) {
+            $ret .= 'Buzz';
+        }
+
+        return $ret ?: $number;
+    }
+}
